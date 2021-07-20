132113f5d2954907dbae78fac1df559341c91f7d
Thiago Bissoli <thiago.bissoli@esales.poa>
2021-07-20 10:15:23 -0300

    Projeto criado

---
+++ Suporte/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Suporte.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
